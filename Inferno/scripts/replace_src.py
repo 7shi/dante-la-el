241bdd9b523004e29419a936e13fbe95a92641b1
7shi <7shi@live.jp>
2023-11-13 17:23:19 +0900

    modify script

--- Inferno/scripts/replace_src.py
+++ Inferno/scripts/replace_src.py
@@ -4,16 +4,16 @@ from striplog import *
 args = sys.argv[1:]
 
 if len(args) != 1:
-    print(f"Usage: python {sys.argv[0]} it")
+    print(f"Usage: python {sys.argv[0]} src")
     sys.exit(1)
 
-it = [line for line in readlines(args[0]) if line]
+src = [line for line in readlines(args[0]) if line]
 
 while (line := sys.stdin.readline()):
     line = chop(line)
     if m := re.match(r"\|\ *(\d+)\ *\|.*?\|(.*?)\|", line):
         ln = int(m.group(1))
         en = m.group(2).strip()
-        print(f"| {ln} | {it[ln-1]} | {en} |")
+        print(f"| {ln} | {src[ln-1]} | {en} |")
     else:
         print(line)
