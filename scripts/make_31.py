6cc8c28dd618609ad0580a2df13d3aac1c137247
7shi <7shi@live.jp>
2023-12-02 20:51:12 +0900

    update script

--- scripts/make_31.py
+++ scripts/make_31.py
@@ -1,23 +1,37 @@
 import sys
 
-if len(sys.argv) != 3:
-    print(f"Usage: python {sys.argv[0]} A B")
+args = sys.argv[1:]
+
+type = 1
+if len(args) >= 2 and args[0] == "-t":
+    type = int(args[1])
+    args = args[2:]
+
+if len(args) != 2:
+    print(f"Usage: python {sys.argv[0]} [-t type] A B")
     sys.exit(1)
 
 texts = []
-for arg in sys.argv[1:]:
+for arg in args:
     with open(arg, "r") as f:
         texts.append([ln for line in f if (ln := line.strip())])
 
-for i in range(0, len(texts[0]), 3):
-    print()
-    e = min(i + 3, len(texts[0]))
-    # if i + 1 == e:
-    #     print(f"# {e}")
-    # else:
-    #     print(f"# {i+1}-{e}")
-    print("[A]")
-    for j in range(i, e):
-        print(texts[0][j])
-    print("[B]")
-    print(texts[1][i // 3])
+i = 0
+while i < len(texts[0]):
+    if i:
+        print()
+    if type == 1:
+        print(texts[0][i])
+        s = i * 3
+        e = min(s + 3, len(texts[1]))
+        for j in range(s, e):
+            print(texts[1][j])
+        i += 1
+    else:
+        print("[A]")
+        e = min(i + 3, len(texts[0]))
+        for j in range(i, e):
+            print(texts[0][j])
+        print("[B]")
+        print(texts[1][i // 3])
+        i += 3
